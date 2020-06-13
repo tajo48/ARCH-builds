a95aa91b198a8ddf46a47de88d6ccdd854b7a840
tajo48 <55502906+tajo48@users.noreply.github.com>
2020-06-14 01:47:21 +0200

    Update config.h

--- dwm/config.h
+++ dwm/config.h
@@ -8,7 +8,7 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "monospace:size=10" };
 static const char dmenufont[]       = "monospace:size=10";
-#include "/home/<USER>/.cache/wal/colors-wal-dwm.h"
+#include "/home/tajo48/.cache/wal/colors-wal-dwm.h"
 
 /* tagging */
 static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
