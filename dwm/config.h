dbcc8107b22c48406c6c08a4f4f003302a4e8013
tajo48 <55502906+tajo48@users.noreply.github.com>
2020-06-27 21:43:43 +0200

    Update config.h

--- dwm/config.h
+++ dwm/config.h
@@ -23,6 +23,7 @@ static const Rule rules[] = {
 	{ "Gimp",    NULL,     NULL,           0,         1,          0,           0,        -1 },
 	{ "Firefox", NULL,     NULL,           1 << 8,    0,          0,          -1,        -1 },
 	{ "st",      NULL,     NULL,           0,         0,          1,          -1,        -1 },
+	{ "tmux",    NULL,     NULL,           0,         0,          1,          -1,        -1 },
 	{ NULL,      NULL,     "Event Tester", 0,         1,          0,           1,        -1 }, /* xev */
 };
 
