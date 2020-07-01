29798741de26b390dc44b8af09bcfdc4bed8d92b
tajo48 <55502906+tajo48@users.noreply.github.com>
2020-07-01 12:40:14 +0200

    Update config.h

--- dwm/config.h
+++ dwm/config.h
@@ -1,12 +1,12 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 0;        /* border pixel of windows */
-static const unsigned int gappx     = 6;        /* gaps between windows */
-static const unsigned int snap      = 32;       /* snap pixel */
+static const unsigned int borderpx  = 5;        /* border pixel of windows */
+static const unsigned int gappx     = 7;        /* gaps between windows */
+static const unsigned int snap      = 16;       /* snap pixel */
 static const int swallowfloating     = 0;        /* 1 means swallow floating windows by default */
 static const int showbar            = 1;        /* 0 means no bar */
-static const int topbar             = 1;        /* 0 means bottom bar */
+static const int topbar             = 0;        /* 0 means bottom bar */
 static const char *fonts[]          = { "monospace:size=10" };
 static const char dmenufont[]       = "monospace:size=10";
 #include "/home/tajo48/.cache/wal/colors-wal-dwm.h"
