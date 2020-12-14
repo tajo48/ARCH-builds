b95790640f1e6d3318a7bd8060673b7855939c1c
tajo48 <tajo161626@gmail.com>
2020-12-14 17:50:02 +0100

    update

--- dwm/config.h
+++ dwm/config.h
@@ -6,7 +6,7 @@ static const unsigned int gappx     = 3;        /* gaps between windows */
 static const unsigned int snap      = 16;       /* snap pixel */
 static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
 static const int showbar            = 1;        /* 0 means no bar */
-static const int topbar             = 0;        /* 0 means bottom bar */
+static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "monospace:size=10" };
 static const char dmenufont[]       = "monospace:size=10";
 #include "/home/tajo48/.cache/wal/colors-wal-dwm.h"
